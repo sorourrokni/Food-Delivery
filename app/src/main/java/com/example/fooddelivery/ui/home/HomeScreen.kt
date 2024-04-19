246d4dceffadd18b14107b20ff3a6fa15767d6b5
sorour <soroorrokni@gmail.com>
2024-04-19 18:07:29 +0330

    Making the main packages of the project

---
+++ app/src/main/java/com/example/fooddelivery/ui/home/HomeScreen.kt
@@ -0,0 +1,12 @@
+package com.example.fooddelivery.ui.home
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun HomeScreen(name: String, modifier: Modifier = Modifier) {
+
+
+}
+
---
+++ app/src/main/java/com/example/fooddelivery/ui/home/history/HistoryScreen.kt
@@ -0,0 +1,4 @@
+package com.example.fooddelivery.ui.home.history
+
+class HistoryScreen {
+}
