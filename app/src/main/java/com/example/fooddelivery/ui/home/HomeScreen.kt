b62009e6f36cb2010699f405d000f960c459f73b
sorour <soroorrokni@gmail.com>
2024-04-21 15:27:38 +0330

    implement top icons, title and tab row in home screen

---
+++ app/src/main/java/com/example/fooddelivery/ui/home/HomeScreen.kt
@@ -0,0 +1,79 @@
+package com.example.fooddelivery.ui.home
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.fooddelivery.R
+import com.example.fooddelivery.component.CustomTabRow
+
+@Composable
+fun HomeScreen(name: String, modifier: Modifier = Modifier) {
+
+    var enabled by remember { mutableStateOf(true) }
+
+    Column {
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(132.dp)
+                .padding(start = 54.dp, top = 74.dp, end = 41.dp, bottom = 0.dp)
+        ) {
+            Image(painter = painterResource(id = R.drawable.ic_more), contentDescription = null)
+            Image(
+                painter = painterResource(id = R.drawable.ic_shopping_cart),
+                contentDescription = null
+            )
+        }
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(132.dp)
+                .padding(start = 54.dp, top = 0.dp, end = 41.dp, bottom = 0.dp)
+        ) {
+            Text(
+                text = "Delicious \n" +
+                        "food for you", style = MaterialTheme.typography.displayLarge
+            )
+        }
+        CustomTabRow(
+            name = "CustomTabRow",modifier= Modifier
+                .fillMaxWidth()
+                .padding(start = 75.dp, top = 80.dp, end = 0.dp, bottom = 0.dp)
+                .height(33.dp)
+        )
+        Row(
+            horizontalArrangement = Arrangement.End,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(0.dp, 0.dp, 41.dp, 40.dp)
+        ) {
+            BasicText(
+                text = "see more",
+                style = MaterialTheme.typography.bodySmall.copy(color = MaterialTheme.colorScheme.primary),
+
+                )
+        }
+    }
+}
+
+
+
+
+
