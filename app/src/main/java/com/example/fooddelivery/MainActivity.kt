77fc92c0f09cc14851c17745a680852fec117486
pashmakhj <83176713+pashmakhj@users.noreply.github.com>
2024-05-01 11:16:40 +0330

    ui component:

--- app/src/main/java/com/example/fooddelivery/MainActivity.kt
+++ app/src/main/java/com/example/fooddelivery/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.example.fooddelivery
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.fooddelivery.ui.history.historyPage
 import com.example.fooddelivery.ui.theme.FoodDeliveryTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +23,18 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             FoodDeliveryTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
+                Column(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
                 ) {
-                    Greeting("Android")
+                    Button(onClick = {
+                        Intent(applicationContext, historyPage::class.java).also {
+                            startActivity(it)
+                        }
+                    }) {
+                        Text("click me")
+                    }
                 }
             }
         }
