d3fb65f2c0c80bf13b1c0e03c675c7bf216efde0
pashmakhj <83176713+pashmakhj@users.noreply.github.com>
2024-05-04 14:07:00 +0330

    deleting extra files

--- app/src/main/java/com/example/fooddelivery/ui/history/historyPage.java
+++
@@ -1,8 +0,0 @@
-package com.example.fooddelivery.ui.history;
-import androidx.activity.ComponentActivity;
-
-
-class historyPage : ComponentActivity(){
-
-
-}
--- app/src/main/java/com/example/fooddelivery/ui/home/history/HistoryScreen.kt
+++ app/src/main/java/com/example/fooddelivery/ui/home/history/HistoryScreen.kt
@@ -1,4 +1,89 @@
-package com.example.fooddelivery.ui.home.history
+package com.example.fooddelivery.ui.history
 
-class HistoryScreen {
+import androidx.compose.Composable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.fooddelivery.R
+import com.example.fooddelivery.component.CustomTabRow
+import com.example.fooddelivery.component.HistoryItem
+import com.example.fooddelivery.data.Address
+import com.example.fooddelivery.data.Delivery
+import com.example.fooddelivery.data.Food
+import com.example.fooddelivery.data.Payment
+import com.example.fooddelivery.data.payment_method
+
+@androidx.compose.runtime.Composable
+fun HistoryScreen(name: String, modifier: Modifier = Modifier){
+    var enabled by remember { mutableStateOf(true)}
+    val scrollState = rememberScrollState()
+
+    Column(
+        modifier = modifier
+            .verticalScroll(scrollState)
+            .background(color = Color.White)
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(132.dp)
+                .padding(start = 54.dp, top = 74.dp, end = 41.dp, bottom = 0.dp)
+        ) {
+            Image(painter = painterResource(id = R.drawable.ic_more), contentDescription = null)
+            Image(
+                painter = painterResource(id = R.drawable.ic_shopping_cart),
+                contentDescription = null
+            )
+        }
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(132.dp)
+                .padding(start = 54.dp, top = 0.dp, end = 41.dp, bottom = 0.dp)
+        ) {
+            Text(
+                text = "History", style = MaterialTheme.typography.displayLarge,color=Color.Black
+            )
+        }
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.background(color = Color.White)
+                .fillMaxWidth()
+                .height(200.dp)
+                .padding(start = 54.dp, top = 0.dp, end = 41.dp, bottom = 0.dp)
+        ) {
+            val food_list=LinkedHashMap<Food,Int>()
+            val food=Food("kebab","1N_199",R.drawable.food_1)
+            val food_2=Food("sdf","skldf",R.drawable.food_3)
+            food_list.put(food,3)
+            food_list.put(food_2,120)
+            val payment_methond= payment_method.Card
+            val address=Address("hooman","honarvar","09358340536","iran shiraz adelAbad lsdfjksldf")
+            val payment=Payment(food_list,1000,address,Delivery.PickUp,payment_methond)
+
+            HistoryItem(payment)
+        }
+    }
+
+
 }
