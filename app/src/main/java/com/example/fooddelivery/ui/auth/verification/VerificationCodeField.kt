8a47231aa718475abc5dfc90c58b329140627475
pashmakhj <83176713+pashmakhj@users.noreply.github.com>
2024-05-11 23:15:16 +0330

    adding verficationCodeField component and VerificationScreen

---
+++ app/src/main/java/com/example/fooddelivery/ui/auth/verification/VerificationCodeField.kt
@@ -0,0 +1,79 @@
+package com.example.fooddelivery.ui.auth.verification
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun VerificationCodeField(time:Int):String {
+    var number by rememberSaveable { mutableStateOf("") }
+    BasicTextField(value = number,
+        onValueChange = {
+            if(it.length<=6){
+                number=it
+            }
+        }, keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.NumberPassword)
+    ) {
+        Row(horizontalArrangement = Arrangement.SpaceBetween
+        , modifier = Modifier.wrapContentSize(Alignment.Center).fillMaxWidth().padding(start=30.dp,top=0.dp, bottom = 0.dp,end=30.dp)
+                    ){
+            repeat(time){index->
+                val char =when{
+                    index>=number.length->""
+                    else->number[index].toString()
+                }
+                Text(modifier = Modifier
+                    .width(64.dp)
+                    .drawBehind {
+                        val strokeWidthPx = 3.dp.toPx()
+                        val verticalOffset = size.height
+                        drawLine(
+                            color = Color(0xffFA4A0C),
+                            strokeWidth = strokeWidthPx,
+                            start = Offset(0f, verticalOffset),
+                            end = Offset(size.width, verticalOffset)
+                        )
+                    },
+                    text = char,
+                    style = MaterialTheme.typography.titleSmall.copy(textDecoration = TextDecoration.None)
+                , textAlign = TextAlign.Center,
+                    )
+                Spacer(modifier = Modifier.width(4.dp))
+
+            }
+        }
+    }
+    return number
+
+
+}
--- app/src/main/java/com/example/fooddelivery/ui/auth/verification/VerificationScreen.kt
+++ app/src/main/java/com/example/fooddelivery/ui/auth/verification/VerificationScreen.kt
@@ -1,8 +1,120 @@
 package com.example.fooddelivery.ui.auth.verification
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.dp
+import com.example.fooddelivery.R
 
 @Composable
 fun VerificationScreen(name:String,modifier: Modifier =Modifier) {
+    val scrollState = rememberScrollState()
+    Column(
+        modifier = Modifier
+            .verticalScroll(scrollState)
+            .background(color = MaterialTheme.colorScheme.onSecondary)
+    ) {
+        Box(
+            modifier = Modifier
+                .height(382.dp)
+                .fillMaxWidth()
+                .padding(bottom = 64.dp)
+                .background(
+                    color = MaterialTheme.colorScheme.onPrimary,
+                    shape = RoundedCornerShape(30.dp).copy(
+                        topStart = CornerSize(0.dp),
+                        topEnd = CornerSize(0.dp)
+                    )
+                )
+        )
+        {
+            Column (modifier= Modifier
+                .align(Alignment.Center)
+                .wrapContentSize(Alignment.Center)
+
+            ){
+                Row(modifier=Modifier.fillMaxWidth().wrapContentSize(Alignment.Center)){
+                Image(
+                    painter = painterResource(id = R.drawable.vuesax),
+                    contentDescription = null,
+                    modifier = Modifier
+//                    .align(Alignment.Center)
+                        .size(100.dp, 100.dp)
+                        .padding( top = 32.dp)
+                )
+
+                }
+                Row(modifier=Modifier.fillMaxWidth().wrapContentSize(Alignment.Center)){
+
+                Text(text = "\nVerification", style = MaterialTheme.typography.titleMedium, color = Color(0xffFA4A0C)
+                )}
+
+                Row(modifier=Modifier.fillMaxWidth().wrapContentSize(Alignment.Center)) {
+
+                    Text(
+                        text = "\nEnter your verification code",
+                        style = MaterialTheme.typography.labelLarge,
+                    )
+                }
+            }
+        }
+        Spacer(modifier = Modifier.height(50.dp))
+        var number=VerificationCodeField(4).take(4)
+        Spacer(modifier = Modifier.height(280.dp))
+
+        FilledButton(onClick = { /*TODO*/ }, text = "Submit")
+
+    }
+}
+
+
+
+@Composable
+fun FilledButton(onClick: () -> Unit, text: String) {
+    Button(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 50.dp, top = 0.dp, end = 50.dp, bottom = 42.dp),
+        onClick = { onClick() },
+        colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary)
+    ) {
+        Text(
+            modifier = Modifier.padding(start = 0.dp, top = 12.dp, end = 0.dp, bottom = 12.dp),
+            text = text,
+            style = MaterialTheme.typography.titleSmall.copy(color = MaterialTheme.colorScheme.secondary)
+        )
+    }
 }
