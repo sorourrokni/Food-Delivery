fd112dbd861d92f2b85bc98a2b0fc6b636509aed
sorour <soroorrokni@gmail.com>
2024-05-12 21:07:11 +0330

    add logo to res directory

--- app/src/main/java/com/example/fooddelivery/MainActivity.kt
+++ app/src/main/java/com/example/fooddelivery/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.fooddelivery
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,43 +10,24 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.room.Room
-import androidx.room.RoomDatabase
-import com.example.fooddelivery.data.DataBase
-import com.example.fooddelivery.data.Food
-import com.example.fooddelivery.data.Person
 import com.example.fooddelivery.theme.FoodDeliveryTheme
 import com.example.fooddelivery.ui.auth.forgotpassword.ForgotPasswordScreen
-import com.example.fooddelivery.ui.auth.onboarding.OnBoardingScreen
-import java.lang.Exception
+import com.example.fooddelivery.ui.auth.login.LoginScreen
+import com.example.fooddelivery.ui.auth.lostconnection.LostConnectionScreen
+import com.example.fooddelivery.ui.auth.verification.VerificationScreen
+import com.example.fooddelivery.ui.home.HomeScreen
 
 class MainActivity : ComponentActivity() {
-    private val db by lazy {    Room.databaseBuilder(
-        applicationContext,
-        DataBase::class.java,
-        "projectDB"
-    ).build()}
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
         setContent {
-
             FoodDeliveryTheme {
-
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val food=Food("Veggie tomato mix","description", "N1,900", R.drawable.food_1)
-                    try {
-                        db.foodDao().insertFood(food)
-                    }
-                    catch (E:Exception){
-                        Log.i("hooman",E.toString())
-                    }
+                 LoginScreen(name = "login")
                 }
             }
         }
