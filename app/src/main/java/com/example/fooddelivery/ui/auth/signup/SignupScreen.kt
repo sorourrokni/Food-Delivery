87202ec22e3350b6f3091c6d836ec896e6dd2ef0
sorour <soroorrokni@gmail.com>
2024-05-13 00:32:40 +0330

    implement auth layout

--- app/src/main/java/com/example/fooddelivery/ui/auth/signup/SignupScreen.kt
+++ app/src/main/java/com/example/fooddelivery/ui/auth/signup/SignupScreen.kt
@@ -1,4 +1,32 @@
 package com.example.fooddelivery.ui.auth.signup
 
-class SignupScreen {
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import com.example.fooddelivery.component.CustomTextField
+import com.example.fooddelivery.component.FilledButton
+
+@Composable
+fun SignupScreen(name: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier
+            .background(color = MaterialTheme.colorScheme.onSecondary)
+    ) {
+        CustomTextField(KeyboardType.Email, label = "Email address")
+
+        Spacer(modifier = Modifier.height(46.dp))
+
+        CustomTextField(KeyboardType.Password, label = "Password")
+
+        Spacer(modifier = Modifier.height(190.dp))
+
+        FilledButton(onClick = { /*TODO*/ }, text = "Sign-up")
+
+    }
 }
