b6f0576f75217db74a39c108ef7e64d8aaee1ffa
sorour <soroorrokni@gmail.com>
2024-05-18 02:29:39 +0330

    implement food detail screen

--- app/src/main/java/com/example/fooddelivery/MainActivity.kt
+++ app/src/main/java/com/example/fooddelivery/MainActivity.kt
@@ -11,8 +11,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.fooddelivery.theme.FoodDeliveryTheme
+import com.example.fooddelivery.ui.home.fooddetail.FoodDetailScreen
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,6 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    FoodDetailScreen(name = "FoodDetail")
                 }
             }
         }
--- app/src/main/java/com/example/fooddelivery/ui/home/fooddetail/FoodDetailScreen.kt
+++ app/src/main/java/com/example/fooddelivery/ui/home/fooddetail/FoodDetailScreen.kt
@@ -1,4 +1,84 @@
 package com.example.fooddelivery.ui.home.fooddetail
 
-class FoodDetailScreen {
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.fooddelivery.R
+import com.example.fooddelivery.component.FilledButton
+import com.example.fooddelivery.component.FoodCaption
+import com.example.fooddelivery.component.ImageSlider
+import com.example.fooddelivery.data.Food
+
+@Composable
+fun FoodDetailScreen(name: String, modifier: Modifier = Modifier) {
+
+    val scrollState = rememberScrollState()
+
+    val foodItem = Food(
+        "Veggie tomato mix",
+        "N1,900",
+        listOf(
+            R.drawable.veggie_tomato_mix1,
+            R.drawable.veggie_tomato_mix2,
+            R.drawable.veggie_tomato_mix3
+        )
+    )
+
+    Column(
+        modifier = modifier
+            .verticalScroll(scrollState)
+            .background(color = MaterialTheme.colorScheme.secondary)
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(132.dp)
+                .padding(start = 42.dp, top = 60.dp, end = 41.dp, bottom = 0.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.chevron_left),
+                contentDescription = null,
+                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primaryContainer)
+            )
+            Image(
+                painter = painterResource(id = R.drawable.ic_heart_unselected),
+                contentDescription = null,
+                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primaryContainer)
+            )
+        }
+        ImageSlider(images = foodItem.imageResIds)
+
+        Column(modifier = Modifier.padding(start = 64.dp, end = 64.dp)) {
+            FoodCaption(
+                title = "Delivery info",
+                caption = "Delivered between monday aug and thursday 20 from 8pm to 91:32 pm"
+            )
+            Spacer(modifier=Modifier.height(42.dp))
+            FoodCaption(
+                title = "Return policy",
+                caption = "Delivered between monday aug and thursday 20 from 8pm to 91:32 pm"
+            )
+        }
+
+        Spacer(modifier=Modifier.height(36.dp))
+
+        FilledButton(onClick = { /*TODO*/ }, text = "Add to cart")
+    }
 }
+
+
