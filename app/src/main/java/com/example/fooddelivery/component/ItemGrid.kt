82dce50ae0838a6b4d8f05d530e007f75255a77c
sorour <soroorrokni@gmail.com>
2024-05-18 13:19:29 +0330

    implement ExpandableHomeScreen using ItemGrid

--- app/src/main/java/com/example/fooddelivery/component/ItemGrid.kt
+++ app/src/main/java/com/example/fooddelivery/component/ItemGrid.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -28,7 +29,9 @@ fun ItemGrid() {
         columns = GridCells.Fixed(2),
         contentPadding = PaddingValues(16.dp,16.dp),
         modifier = Modifier
-            .background(color = MaterialTheme.colorScheme.onSecondary)
+            .background(color = MaterialTheme.colorScheme.onSecondary, shape = RoundedCornerShape(30.dp))
+            .padding(top = 36.dp)
+
     ) {
         items(foodItems.size) { index ->
             val topPadding = if (index % 2 == 0) 0.dp else 54.dp
--- app/src/main/java/com/example/fooddelivery/ui/home/expandable/ExpandableHomeScreen.kt
+++ app/src/main/java/com/example/fooddelivery/ui/home/expandable/ExpandableHomeScreen.kt
@@ -1,14 +1,41 @@
 package com.example.fooddelivery.ui.home.expandable
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.Group
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.fooddelivery.R
+import com.example.fooddelivery.component.ItemGrid
 
 @Composable
 fun ExpandableHomeScreen(name: String, modifier: Modifier = Modifier) {
-    Group {
-
+    Column(modifier = Modifier.background(color = MaterialTheme.colorScheme.tertiaryContainer)) {
+        Row(
+            horizontalArrangement = Arrangement.Start,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 42.dp, top = 64.dp, bottom = 36.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.chevron_left),
+                contentDescription = null
+            )
+            Text(
+                modifier = Modifier.padding(start = 36.dp),
+                text = "See more",
+                style = MaterialTheme.typography.bodyMedium.copy(fontSize = 18.sp)
+            )
+        }
+        ItemGrid()
     }
 }
