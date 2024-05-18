8c08ad884bfcc29886e1ede8df4f03d9aac3dce0
sorour <soroorrokni@gmail.com>
2024-05-18 04:28:25 +0330

    implement Item grid component

---
+++ app/src/main/java/com/example/fooddelivery/component/ItemGrid.kt
@@ -0,0 +1,51 @@
+package com.example.fooddelivery.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.fooddelivery.R
+import com.example.fooddelivery.data.Food
+
+@Composable
+fun ItemGrid() {
+
+    val foodItems = listOf(
+        Food("Veggie tomato mix", "N1,900", R.drawable.food_1),
+        Food("Egg and cucumber", "N1,900", R.drawable.food_2),
+        Food("Egg and cucumber", "N1,900", R.drawable.food_3),
+        Food("Egg and cucumber", "N1,900", R.drawable.food_4),
+        Food("Egg and cucumber", "N1,900", R.drawable.food_1),
+    )
+
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        contentPadding = PaddingValues(16.dp,16.dp),
+        modifier = Modifier
+            .background(color = MaterialTheme.colorScheme.onSecondary)
+    ) {
+        items(foodItems.size) { index ->
+            val topPadding = if (index % 2 == 0) 0.dp else 54.dp
+            Box(
+                modifier = Modifier
+                    .padding(top = topPadding)
+            ) {
+                FoodItem(
+                    foodItem = foodItems[index],
+                    width = 156,
+                    topHeight = 252,
+                    height = 212,
+                    titlePadding = 8,
+                    imgPadding = 0,
+                    scale = 1.2f
+                )
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/fooddelivery/ui/home/expandable/ExpandableHomeScreen.kt
+++ app/src/main/java/com/example/fooddelivery/ui/home/expandable/ExpandableHomeScreen.kt
@@ -1,4 +1,14 @@
 package com.example.fooddelivery.ui.home.expandable
 
-class ExpandableHomeScreen {
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.Group
+
+@Composable
+fun ExpandableHomeScreen(name: String, modifier: Modifier = Modifier) {
+    Group {
+
+    }
 }
