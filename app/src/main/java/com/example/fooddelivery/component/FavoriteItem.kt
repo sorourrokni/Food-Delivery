e2229218d5d0acbf4e7ee3c199e7cb3e575e154b
sorour <soroorrokni@gmail.com>
2024-05-23 02:25:06 +0330

    fix heart icon issue in FavoriteItem

--- app/src/main/java/com/example/fooddelivery/component/FavoriteItem.kt
+++ app/src/main/java/com/example/fooddelivery/component/FavoriteItem.kt
@@ -7,14 +7,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.fooddelivery.R
@@ -47,7 +50,8 @@ fun FavoriteItem(food: Food, modifier: Modifier = Modifier) {
         Column(
             modifier = Modifier
                 .fillMaxHeight()
-                .padding(top = 16.dp, end = 16.dp)
+                .fillMaxWidth()
+                .padding(top = 16.dp, end = 16.dp, bottom = 16.dp)
         ) {
             Text(
                 text = food.name,
@@ -59,21 +63,14 @@ fun FavoriteItem(food: Food, modifier: Modifier = Modifier) {
                 style = MaterialTheme.typography.titleSmall,
                 color = MaterialTheme.colorScheme.primary
             )
-        }
-        Column(
-            modifier = Modifier
-                .fillMaxHeight()
-                .padding(bottom = 24.dp, end = 24.dp),
-            verticalArrangement = Arrangement.Bottom
-        ) {
             Image(
                 painter = painterResource(id = R.drawable.ic_heart_selected),
                 contentDescription = null,
+                colorFilter = ColorFilter.tint(color = MaterialTheme.colorScheme.primary),
                 modifier = Modifier
-
+                    .align(Alignment.End)
+                    .padding(end = 16.dp)
             )
         }
-
     }
-
 }
