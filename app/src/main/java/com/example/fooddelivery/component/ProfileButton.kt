45659132678a2f25ffc7f98d0c55623a11e19536
sorour <soroorrokni@gmail.com>
2024-05-23 02:45:02 +0330

    fix not showing icon in ProfileButton

--- app/src/main/java/com/example/fooddelivery/component/ProfileButton.kt
+++ app/src/main/java/com/example/fooddelivery/component/ProfileButton.kt
@@ -1,16 +1,17 @@
 package com.example.fooddelivery.component
 
-import android.widget.Space
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -19,16 +20,27 @@ import androidx.compose.ui.unit.sp
 import com.example.fooddelivery.R
 
 @Composable
-fun ProfileButton(name:String,modifier:Modifier=Modifier){
+fun ProfileButton(name: String, modifier: Modifier = Modifier) {
     Row(
         modifier
+            .fillMaxWidth()
             .size(315.dp, 60.dp)
-            .background(color = Color.White, shape = RoundedCornerShape(20.dp))){
-        Row( modifier=Modifier.padding(start=23.dp, top = 20.dp, bottom = 19.dp)){
-            Text(name,style=MaterialTheme.typography.headlineSmall.copy(fontSize = 18.sp, lineHeight = 21.sp)
-            ,modifier=Modifier.size(151.dp,21.dp))
-            Image(painter = painterResource(id = R.drawable.chevron_right), contentDescription = null
-            ,modifier=Modifier.padding(start=90.dp))
-        }
+            .background(color = Color.White, shape = RoundedCornerShape(20.dp)),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            modifier = Modifier.padding(start = 24.dp),
+            text = name,
+            style = MaterialTheme.typography.headlineSmall.copy(
+                fontSize = 18.sp,
+                lineHeight = 21.sp
+            )
+        )
+        Image(
+            painter = painterResource(id = R.drawable.chevron_right),
+            contentDescription = null,
+            modifier = Modifier.padding(end = 16.dp)
+        )
     }
 }
