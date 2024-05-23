6ea6c357f382b5d743ad46d658c2caba87a019d6
pashmakhj <83176713+pashmakhj@users.noreply.github.com>
2024-05-23 12:33:57 +0330

    updating profileViewModel

--- app/src/main/java/com/example/fooddelivery/viewModel/profileViewModel.kt
+++ app/src/main/java/com/example/fooddelivery/viewModel/profileViewModel.kt
@@ -3,11 +3,14 @@ package com.example.fooddelivery.viewModel;
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.example.fooddelivery.data.Address
 import com.example.fooddelivery.data.Person
 import com.example.fooddelivery.data.dao.AddressDao
 import com.example.fooddelivery.data.dao.PersonDao
 import com.example.fooddelivery.repository.AddressRepository
 import com.example.fooddelivery.repository.PersonRepository
+import kotlinx.coroutines.launch
 
 class profileViewModel(
     private val personRepository: PersonRepository,
@@ -16,7 +19,11 @@ class profileViewModel(
     : ViewModel() {
     fun getProfileInfo(email: String): Person? {
         try {
-            return personDao.getPersonWithEmail(email)
+            var person:Person?=null
+            viewModelScope.launch {
+                person=personRepository.getPersonWithEmail(email)
+            }
+            return person
         } catch (e: Exception) {
             Log.i("getProfileError", e.toString())
             return null
@@ -24,20 +31,32 @@ class profileViewModel(
     }
 
     fun updateProfileInfo(name: String, newEmail: String, phone: String, address: String): Boolean {
-        val person = personDao.getPersonWithEmail(newEmail)
-        if (person == null) {
-            val oldPerson = personDao.getPersonWithEmail(email)
+        var person:Person?=null
+        viewModelScope.launch {
+            person=personRepository.getPersonWithEmail(email)
+        }
+        if (person != null) {
+            var oldPerson:Person?=null
+            viewModelScope.launch {
+                oldPerson=personRepository.getPersonWithEmail(email)
+            }
+
             if (oldPerson != null) {
-                oldPerson.email = newEmail
-                oldPerson.fullName = name
-                oldPerson.phoneNumber = phone
-                val addressObject = addressDao.getUserAddress(email)
-                if (addressObject != null) {
-                    addressObject.description = address
-                    addressObject.user = newEmail
-                    addressDao.upsertAddress(addressObject)
+                oldPerson!!.email = newEmail
+                oldPerson?.fullName = name
+                oldPerson?.phoneNumber = phone
+                var addressObject:Address?=null
+                viewModelScope.launch {
+                    addressObject=addressRepository.getUserAddress(email)
                 }
-                personDao.upsertPerson(oldPerson)
+                if (addressObject != null) {
+                    addressObject?.description = address
+                    addressObject?.user = newEmail
+                    viewModelScope.launch{
+                        addressRepository.upsert(addressObject!!)
+                    }
+                }
+                viewModelScope.launch { personRepository.upsert(oldPerson!!) }
                 return true
 
             } else {
