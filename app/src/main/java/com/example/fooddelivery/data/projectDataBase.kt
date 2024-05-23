9be604fb45bcbcadb1fee40758be14a78331589c
pashmakhj <83176713+pashmakhj@users.noreply.github.com>
2024-05-23 10:02:44 +0330

    change database to singleTon

--- app/src/main/java/com/example/fooddelivery/data/projectDataBase.kt
+++ app/src/main/java/com/example/fooddelivery/data/projectDataBase.kt
@@ -1,6 +1,8 @@
 package com.example.fooddelivery.data
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.fooddelivery.data.dao.AddressDao
 import com.example.fooddelivery.data.dao.FoodDao
@@ -20,4 +22,27 @@ abstract class DataBase:RoomDatabase() {
     abstract fun orderDao():OrderDao
     abstract fun orderItemDao():OrderItemDao
     abstract fun personDao():PersonDao
+
+
+    companion object {
+        // Singleton prevents multiple instances of database opening at the
+        // same time.
+        @Volatile
+        private var INSTANCE:DataBase? = null
+
+        fun getDatabase(context: Context): DataBase {
+            // if the INSTANCE is not null, then return it,
+            // if it is, then create the database
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    DataBase::class.java,
+                    "project_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+
 }
